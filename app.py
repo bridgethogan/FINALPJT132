c784030a73438715fb65dc4be5233d7573c2de56
bridgethogan <149599405+bridgethogan@users.noreply.github.com>
2023-12-06 15:59:39 +0000

    htmlcss

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello():
-    return 'Hello, Flask!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
+
+
+
 
